75a3b732581f6017fb7a311ad4b63f65ad8a0269
squirrelAf <aleckraowo@gmail.com>
2025-06-01 23:09:55 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <string>
+
+extern void makeTree(BST<std::string>& tree, const char* filename);Add commentMore actions
+extern void displayFrequency(BST<std::string>& tree);
 
 int main() {
+  BST<std::string> wordTree;Add commentMore actions
+    const char* inputFile = "src/war_peace.txt";
+
+    makeTree(wordTree, inputFile);
+    displayFrequency(wordTree);
+
+    std::cout << "Depth of the BST: " << wordTree.height() << std::endl;
+
     return 0;
 }
